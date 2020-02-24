fa06a27d68a51d497c2f1fa2c43b221349cea90d
Himanshu Chaudhary <34539445+himanshuamd1@users.noreply.github.com>
2020-02-24 22:59:34 +0530

    Delete Ones.py

--- Ones.py
+++
@@ -1,9 +0,0 @@
-def Ones(mat,R,C): 
-  
-    perimeter = 0; 
-    for i in range(0, R): 
-        for j in range(0, C): 
-            if (mat[i][j]==0): 
-                perimeter += (1); 
-  
-    return perimeter; 
